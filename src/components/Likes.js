075300850661494dcdd99f35473ea65c6e9c14d1
alibek <mambetkerimovalibek7@gmail.com>
2022-03-02 23:19:37 +0600

    first commit

--- src/components/Likes.js
+++ src/components/Likes.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import { connect } from "react-redux";
+import {incrementLikes, decrementLikes} from '../redux/actions'
 
 const Likes = (props) => {
+    console.log(props)
+
+    const classes = {
+        padding: "19px 50px",
+        border: "3px solid blue",
+        background: "transparent",
+        color: "blue",
+        fontSize: "30px",
+        textTransform: "uppercase",
+        cursor: "pointer"
+    }
+
+    const controlClasses = {
+        display: "flex",
+        justifyContent: "center",
+        gap: "135px",
+        width: "100%",
+        marginTop: "50px"
+    }
+
     return (
-        <div className="button-contorls">
-            <button>❤{props.likes}</button>
-            <button>Dislike</button>
+        <div className="button-contorls" style={controlClasses}>
+            <button onClick={props.onIncrementLikes} style={classes}>❤ {props.likes}</button>
+            <button onClick={props.onDecrementLikes} style={classes}>Dislike</button>
         </div>
     );
 };
 
-export default Likes;
+function mapStateToProps(state) {
+    console.log('mapStateToProps >', state);
+    const { likesReducer } = state;
+    return {
+        likes: likesReducer.likes
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        onIncrementLikes: () => {
+            return dispatch(incrementLikes())
+        },
+        onDecrementLikes: () => {
+            return dispatch(decrementLikes())
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Likes);
